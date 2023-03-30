1137a8f5a28831ab48d089476640666a65a92bf5
Boncsy <koharibarbara@gmail.com>
2023-03-30 13:55:00 +0200

    vizsgaremek commit

---
+++ test_conduit_vizsgaremek/test_registration.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import csv
+from general_functions import login, create_article, create_comment
+from general_data import user, article, comment
+
+class TestConduit(object):
+    def setup(self):
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        self.browser = webdriver.Chrome(service=service, options=options)
+        URL = "http://localhost:1667/"
+        self.browser.get(URL)
+        self.browser.maximize_window()
+
+    def teardown(self):
+        self.browser.quit()
+
+
+# ATC000 - Regisztráció helyes adattal
+    def test_registration(self):
+        sign_up_button = self.browser.find_element(By.LINK_TEXT, 'Sign up')
+        sign_up_button.click()
+
+        username_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Username"]')
+        email_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+        password_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
+
+        username_input.send_keys(user["name"])
+        email_input.send_keys(user["email"])
+        password_input.send_keys(user["password"])
+
+        sign_up_button2 = self.browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+        sign_up_button2.click()
+
+        time.sleep(2)
+
+        registration_message = self.browser.find_element(By.XPATH, '//div[@class="swal-title"]')
+        registration_problem = self.browser.find_element(By.XPATH, '//div[@class="swal-text"]')
+        assert registration_message.text == "Welcome!"
+        assert registration_problem.text == "Your registration was successful!"
+
+        registration_ok_button = self.browser.find_element(By.XPATH, '//button[@class="swal-button swal-button--confirm"]')
+        registration_ok_button.click()
+
+        time.sleep(2)
+
+        user_profile = self.browser.find_elements(By.XPATH, '//a[@class="nav-link"]')[2]
+        assert user_profile.text == user["name"]

194346994b146212cc8627902e826f9ddc52f9c6
Boncsy <koharibarbara@gmail.com>
2023-04-04 10:56:06 +0200

    vizsgaremek commit

--- test_conduit_vizsgaremek/test_conduit_vizsgaremek.py
+++ test_conduit_vizsgaremek/test_conduit_vizsgaremek.py
@@ -31,7 +31,7 @@ class TestConduit(object):
 
 # ATC001 - Adatkezelési tájékoztató
     def test_accept_cookies(self):
-        accept_button = self.browser.find_element(By.XPATH, '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
+        accept_button = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')))
         accept_button.click()
         decline_button_list = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--decline"]')))
         assert not len(decline_button_list)
@@ -216,7 +216,7 @@ class TestConduit(object):
         article_tags_input.send_keys()
         publish_article_button.click()
 
-        error_message_button = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, '//button[@class="swal-button swal-button--confirm"]')))
+        error_message_button = WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.XPATH, '//button[@class="swal-button swal-button--confirm"]')))
         assert error_message_button.is_displayed()
         error_message_button.click()
 

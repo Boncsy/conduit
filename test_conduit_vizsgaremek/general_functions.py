85cfed872a3f940043420c422e1c29e4072c6146
Boncsy <koharibarbara@gmail.com>
2023-04-04 09:47:06 +0200

    vizsgaremek commit

--- test_conduit_vizsgaremek/general_functions.py
+++ test_conduit_vizsgaremek/general_functions.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 def login(browser, user_email, user_password):
     sign_in_page_button = browser.find_element(By.XPATH, '//a[@href="#/login"]')
@@ -9,7 +11,7 @@ def login(browser, user_email, user_password):
     password_input = browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
     password_input.send_keys(user_password)
 
-    sign_in_button = browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+    sign_in_button = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')))
     sign_in_button.click()
 
 
@@ -17,9 +19,7 @@ def create_article(browser, title_input, about_input, main_input, tag_input):
     new_article_button = browser.find_element(By.XPATH, '//a[@href="#/editor"]')
     new_article_button.click()
 
-    time.sleep(2)
-
-    article_title_input = browser.find_element(By.XPATH, '//input[@placeholder="Article Title"]')
+    article_title_input = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]')))
     article_about_input = browser.find_element(By.XPATH, '//input[starts-with(@placeholder,"What")]')
     article_main_input = browser.find_element(By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')
     article_tags_input = browser.find_element(By.XPATH, '//input[@placeholder="Enter tags"]')

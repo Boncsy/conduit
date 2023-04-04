3633cb8e44abad8f7f689067e44becf303bbdf83
Boncsy <koharibarbara@gmail.com>
2023-04-04 12:11:40 +0200

    vizsgaremek commit

--- test_conduit_vizsgaremek/general_functions.py
+++ test_conduit_vizsgaremek/general_functions.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
+import time
 def login(browser):
     sign_in_page_button = browser.find_element(By.XPATH, '//a[@href="#/login"]')
     sign_in_page_button.click()
@@ -18,7 +18,7 @@ def create_article(browser):
     new_article_button = browser.find_element(By.XPATH, '//a[@href="#/editor"]')
     new_article_button.click()
 
-    article_title_input = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]')))
+    article_title_input = WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]')))
     article_about_input = browser.find_element(By.XPATH, '//input[starts-with(@placeholder,"What")]')
     article_main_input = browser.find_element(By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')
     article_tags_input = browser.find_element(By.XPATH, '//input[@placeholder="Enter tags"]')
@@ -28,12 +28,12 @@ def create_article(browser):
     article_main_input.send_keys("Ide írjuk magának a cikknek a tartalmát")
     article_tags_input.send_keys("testtags")
     publish_article_button.click()
-
+    time.sleep(5)
 def create_article_data(browser, title_input, about_input, main_input, tag_input):
     new_article_button = browser.find_element(By.XPATH, '//a[@href="#/editor"]')
     new_article_button.click()
 
-    article_title_input = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]')))
+    article_title_input = WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]')))
     article_about_input = browser.find_element(By.XPATH, '//input[starts-with(@placeholder,"What")]')
     article_main_input = browser.find_element(By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')
     article_tags_input = browser.find_element(By.XPATH, '//input[@placeholder="Enter tags"]')
@@ -43,9 +43,10 @@ def create_article_data(browser, title_input, about_input, main_input, tag_input
     article_main_input.send_keys(main_input)
     article_tags_input.send_keys(tag_input)
     publish_article_button.click()
-
+    time.sleep(5)
 def create_comment(browser):
     comment_input = browser.find_element(By.XPATH, '//textarea[@placeholder="Write a comment..."]')
-    post_comment_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-sm btn-primary"]')))
+    post_comment_button = WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-sm btn-primary"]')))
     comment_input.send_keys("Ez egy teszt komment.")
     post_comment_button.click()
+    time.sleep(5)
